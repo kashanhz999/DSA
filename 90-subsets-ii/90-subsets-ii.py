eb38d0ca313331b0dce367dbb4ade98023be81e2
kashanhz999 <97667567+kashanhz999@users.noreply.github.com>
2022-09-10 12:24:41 +0530

    Time: 57 ms (57.48%), Space: 14.1 MB (49.34%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        nums.sort()
+        
+        def backtrack(i, subset):
+            
+            #base_case
+            if(i == len(nums)):
+                res.append(subset[::])
+                return
+            
+            #nums[i] included in subset
+            
+            subset.append(nums[i])
+            backtrack(i+1,subset)
+            subset.pop()
+            
+            #nums[i] not included
+            
+            while i+1<len(nums) and nums[i] == nums[i+1]:
+                i +=1
+            backtrack(i+1,subset)
+        backtrack(0,[])
+        return res
+        
+    
+            
+            
+        
